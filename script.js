e0ad2cbd7520a75182365388f364200de6969472
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-03-01 12:11:57 -0400

    clean code - using padStart method

--- script.js
+++ script.js
@@ -15,41 +15,27 @@ let hours = 0;
 
 function startTime() {
   sec++;
-  if (sec < 60) {
-    if (sec < 10) {
-      seconds.innerHTML = `0${sec}`;
-    } else if (sec > 9 && sec < 60) {
-      seconds.innerHTML = sec;
-    }
-  } else if (sec > 59) {
-    if (sec == 60) {
-      sec = 0;
-      seconds.innerHTML = `0${sec}`;
-    }
-
+  if (sec > 59) {
+    sec = 0;
     min++;
-    if (min < 10) {
-      minuts.innerHTML = `0${min}`;
-    } else if (min > 9 && min < 60) {
-      minuts.innerHTML = min;
-    } else if (min > 59) {
-      min = 0;
-      minuts.innerHTML = `0${min}`;
-      hours++;
-      if (hours < 10) {
-        createSpan.innerHTML = `0${hours} :`;
-      } else if (hours > 9) {
-        createSpan.innerHTML = `${hours} :`;
-      }
-      time.appendChild(createSpan);
-      time.insertAdjacentElement("afterbegin", createSpan);
-    }
   }
+  seconds.innerHTML = String(sec).padStart(2, '0');
+
+  if (min > 59) {
+    min = 0;
+    hours++;
+
+    createSpan.innerHTML = `${String(hours).padStart(2, '0')} :`;
+    time.appendChild(createSpan);
+    time.insertAdjacentElement("afterbegin", createSpan);
+  }
+
+  minuts.innerHTML = String(min).padStart(2, '0');
 }
 
 let interval;
 start.addEventListener("click", function () {
-  interval = setInterval(startTime, 1000);
+  interval = setInterval(startTime, 10);
 });
 stop.addEventListener("click", function () {
   clearInterval(interval);
